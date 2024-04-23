15dbe0058811f7c34153a79f11dbe09364da514c
Luke Boswell <lukewilliamboswell@gmail.com>
2024-04-23 10:46:12 +1000

    add bundle to build script

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 cargo build
 
-cp target/debug/libroc_pdf_experiment.a macos-arm64.a
+cp platform/target/debug/libroc_pdf_experiment.a platform/macos-arm64.a
+
+# UNCOMMENT to bundle into URL package for distribution
+# roc build --bundle .tar.br platform/main.roc
